1d78b4c1edc0cec45f985586a3253e7285e12340
Gaila Galano <gailag@hawaii.edu>
2022-02-07 22:27:22 -1000

    E27: Fixed version

--- ziplist.js
+++ ziplist.js
@@ -1,18 +1,18 @@
 const list1 = ['a', 'b', 'c'];
 const list2 = [1, 2, 3];
 
-function zipList(list1, list2) {
+function zipList(array1, array2) {
   const finalArray = [];
-  for (let i = 0; i < list1.length; i++) {
-    finalArray.push(list1[i], list2[i]);
+  for (let i = 0; i < array1.length; i++) {
+    finalArray.push(array1[i], array2[i]);
   }
   return finalArray;
 }
 
 console.log(zipList(list1, list2));
 
-function zipListTheSimpleWay(list1, list2) {
-  return _.flatten(_.zip(list1, list2));
+function zipListTheSimpleWay(array1, array2) {
+  return _.flatten(_.zip(array1, array2));
 }
 
 console.log(zipListTheSimpleWay(list1, list2));
